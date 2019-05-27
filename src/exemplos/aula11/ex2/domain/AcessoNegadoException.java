14d124c5dfbdd9b9fa229e12304a89695daabf5b
monteiro <monteiro@motorola.com>
2019-05-27 18:32:48 -0300

    exemplos aula 13

--- src/exemplos/aula11/ex2/domain/AcessoNegadoException.java
+++ src/exemplos/aula11/ex2/domain/AcessoNegadoException.java
@@ -2,9 +2,13 @@ package exemplos.aula11.ex2.domain;
 
 public class AcessoNegadoException extends Exception{
 
-	private static final long serialVersionUID = -2929609067337201914L;
-	
+	private static final long serialVersionUID = 2771475705978096125L;
+
 	public AcessoNegadoException() {
 		super("Você não tem permissão para acessar esta funcionalidade");
+	}
+	
+	public AcessoNegadoException(String msg) {
+		super(msg);
 	}
 }
